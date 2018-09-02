72cbc78aee74dbf5cec26391fb657271d197a629
Ndhaniff <ndhaniff@gmail.com>
2018-09-02 12:12:06 +0800

    fix title issue

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,9 +5,9 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, viewport-fit=cover">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-        <title><?= $title . " | WaSport" ?></title>
+        <title>@yield('title', 'WaSport')</title>
 
-        <!--@yield('title', 'WaSport') -->
+        <!-- $title . " | WaSport"  -->
 
         <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/owl-carousel/1.3.2/owl.carousel.css">
