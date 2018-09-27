78b9f9b0f00964a221b5dadb42b4451522f8c99e
ndhaniff <ndhaniff@gmail.com>
2018-09-27 14:41:02 +0800

    fix email

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://wasportsrun.com'),
+    'url' => env('APP_URL', 'http://wasportsrun.com'),
 
     /*
     |--------------------------------------------------------------------------
