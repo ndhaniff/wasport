cb10d2e19078c239db60b8777bebe901c3a96413
ndhaniff <ndhaniff@gmail.com>
2018-10-05 15:52:57 +0800

    fix races dropzone

--- resources/views/auth/admin/races/edit.blade.php
+++ resources/views/auth/admin/races/edit.blade.php
@@ -8,34 +8,19 @@ Admin | Edit Race
 
 <script>
 
-var awardimg_1 = '[{"preview" : "{{asset("storage/uploaded/awards/".$race->awardimg_1)}}"}]';
-var awardimg_2 = '[{"preview" : "{{asset("storage/uploaded/awards/".$race->awardimg_2)}}"}]';
-var awardimg_3 = '[{"preview" : "{{asset("storage/uploaded/awards/".$race->awardimg_3)}}"}]';
-var awardimg_4 = '[{"preview" : "{{asset("storage/uploaded/awards/".$race->awardimg_4)}}"}]';
-var awardimg_5 = '[{"preview" : "{{asset("storage/uploaded/awards/".$race->awardimg_5)}}"}]';
-var awardimg_6 = '[{"preview" : "{{asset("storage/uploaded/awards/".$race->awardimg_6)}}"}]';
+var award_img1 = JSON.parse('[{"preview" : "{{asset("storage/uploaded/awards/".$race->awardimg_1)}}"}]');
+var award_img2 = JSON.parse('[{"preview" : "{{asset("storage/uploaded/awards/".$race->awardimg_2)}}"}]');
+var award_img3 = JSON.parse('[{"preview" : "{{asset("storage/uploaded/awards/".$race->awardimg_3)}}"}]');
+var award_img4 = JSON.parse('[{"preview" : "{{asset("storage/uploaded/awards/".$race->awardimg_4)}}"}]');
+var award_img5 = JSON.parse('[{"preview" : "{{asset("storage/uploaded/awards/".$race->awardimg_5)}}"}]');
+var award_img6 = JSON.parse('[{"preview" : "{{asset("storage/uploaded/awards/".$race->awardimg_6)}}"}]');
 
-var award_img_1 = JSON.parse(awardimg_1);
-var award_img_2 = JSON.parse(awardimg_2);
-var award_img_3 = JSON.parse(awardimg_3);
-var award_img_4 = JSON.parse(awardimg_4);
-var award_img_5 = JSON.parse(awardimg_5);
-var award_img_6 = JSON.parse(awardimg_6);
-
-var medalimg_1 = '[{"preview" : "{{asset("storage/uploaded/medals/".$race->medalimg_1)}}"}]';
-var medalimg_2 = '[{"preview" : "{{asset("storage/uploaded/medals/".$race->medalimg_2)}}"}]';
-var medalimg_3 = '[{"preview" : "{{asset("storage/uploaded/medals/".$race->medalimg_3)}}"}]';
-var medalimg_4 = '[{"preview" : "{{asset("storage/uploaded/medals/".$race->medalimg_4)}}"}]';
-var medalimg_5 = '[{"preview" : "{{asset("storage/uploaded/medals/".$race->medalimg_5)}}"}]';
-var medalimg_6 = '[{"preview" : "{{asset("storage/uploaded/medals/".$race->medalimg_6)}}"}]';
-
-var medal_img_1 = JSON.parse(medalimg_1);
-var medal_img_2 = JSON.parse(medalimg_2);
-var medal_img_3 = JSON.parse(medalimg_3);
-var medal_img_4 = JSON.parse(medalimg_4);
-var medal_img_5 = JSON.parse(medalimg_5);
-var medal_img_6 = JSON.parse(medalimg_6);
-
+var medal_img1 = JSON.parse('[{"preview" : "{{asset("storage/uploaded/medals/".$race->medalimg_1)}}"}]');
+var medal_img2 = JSON.parse('[{"preview" : "{{asset("storage/uploaded/medals/".$race->medalimg_2)}}"}]');
+var medal_img3 = JSON.parse('[{"preview" : "{{asset("storage/uploaded/medals/".$race->medalimg_3)}}"}]');
+var medal_img4 = JSON.parse('[{"preview" : "{{asset("storage/uploaded/medals/".$race->medalimg_4)}}"}]');
+var medal_img5 = JSON.parse('[{"preview" : "{{asset("storage/uploaded/medals/".$race->medalimg_5)}}"}]');
+var medal_img6 = JSON.parse('[{"preview" : "{{asset("storage/uploaded/medals/".$race->medalimg_6)}}"}]');
 
 var race = {
   rid: "{{$race->rid}}",
@@ -64,24 +49,24 @@ var race = {
   deadtime_from : "{{$race->deadtime_from}}",
   header : ("{{$race->header}}" == '') ? "" : "preview : {{asset('storage/uploaded/races/'.$race->header)}}",
   toggleDrop : ("{{$race->header}}" == '') ? true : false,
-  awardimg_1 : ("{{$race->awardimg_1}}" == '') ? "" : award_img_1,
-  awardimg_2 : ("{{$race->awardimg_2}}" == '') ? "" : award_img_2,
-  awardimg_3 : ("{{$race->awardimg_3}}" == '') ? "" : award_img_3,
-  awardimg_4 : ("{{$race->awardimg_4}}" == '') ? "" : award_img_4,
-  awardimg_5 : ("{{$race->awardimg_5}}" == '') ? "" : award_img_5,
-  awardimg_6 : ("{{$race->awardimg_6}}" == '') ? "" : award_img_6,
+  awardimg_1 : ("{{$race->awardimg_1}}" == '') ? "" : award_img1,
+  awardimg_2 : ("{{$race->awardimg_2}}" == '') ? "" : award_img2,
+  awardimg_3 : ("{{$race->awardimg_3}}" == '') ? "" : award_img3,
+  awardimg_4 : ("{{$race->awardimg_4}}" == '') ? "" : award_img4,
+  awardimg_5 : ("{{$race->awardimg_5}}" == '') ? "" : award_img5,
+  awardimg_6 : ("{{$race->awardimg_6}}" == '') ? "" : award_img6,
   toggleDrop_awardimg_1 : ("{{$race->awardimg_1}}" == '') ? true : false,
   toggleDrop_awardimg_2 : ("{{$race->awardimg_2}}" == '') ? true : false,
   toggleDrop_awardimg_3 : ("{{$race->awardimg_3}}" == '') ? true : false,
   toggleDrop_awardimg_4 : ("{{$race->awardimg_4}}" == '') ? true : false,
   toggleDrop_awardimg_5 : ("{{$race->awardimg_5}}" == '') ? true : false,
   toggleDrop_awardimg_6 : ("{{$race->awardimg_6}}" == '') ? true : false,
-  medalimg_1 : ("{{$race->medalimg_1}}" == '') ? "" : medal_img_1,
-  medalimg_2 : ("{{$race->medalimg_2}}" == '') ? "" : medal_img_2,
-  medalimg_3 : ("{{$race->medalimg_3}}" == '') ? "" : medal_img_3,
-  medalimg_4 : ("{{$race->medalimg_4}}" == '') ? "" : medal_img_4,
-  medalimg_5 : ("{{$race->medalimg_5}}" == '') ? "" : medal_img_5,
-  medalimg_6 : ("{{$race->medalimg_6}}" == '') ? "" : medal_img_6,
+  medalimg_1 : ("{{$race->medalimg_1}}" == '') ? "" : medal_img1,
+  medalimg_2 : ("{{$race->medalimg_2}}" == '') ? "" : medal_img2,
+  medalimg_3 : ("{{$race->medalimg_3}}" == '') ? "" : medal_img3,
+  medalimg_4 : ("{{$race->medalimg_4}}" == '') ? "" : medal_img4,
+  medalimg_5 : ("{{$race->medalimg_5}}" == '') ? "" : medal_img5,
+  medalimg_6 : ("{{$race->medalimg_6}}" == '') ? "" : medal_img6,
   toggleDrop_medalimg_1 : ("{{$race->medalimg_1}}" == '') ? true : false,
   toggleDrop_medalimg_2 : ("{{$race->medalimg_2}}" == '') ? true : false,
   toggleDrop_medalimg_3 : ("{{$race->medalimg_3}}" == '') ? true : false,
