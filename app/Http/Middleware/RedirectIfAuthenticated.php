4791579bf972c3b006d349c281364d1961a3dd9a
ndhaniff <ndhaniff@gmail.com>
2018-12-28 13:49:01 +0800

    update login

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -4,7 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Session;
-use Redirect;
+use App\Http\Controllers\Auth\Redirect;
 use Illuminate\Support\Facades\Auth;
 
 class RedirectIfAuthenticated
