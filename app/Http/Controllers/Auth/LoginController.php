8c6fcd5b33c33d4e4dd8a7e51a897fef968826b8
ndhaniff <ndhaniff@gmail.com>
2018-12-28 10:33:45 +0800

    update login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -30,7 +30,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    //protected $redirectTo = '/dashboard';
+    protected $redirectTo = '/dashboard';
 
     protected function authenticated(Request $request, $user)
     {
