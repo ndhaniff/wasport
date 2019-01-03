5d8a061deff7f1b375a0e4322d2e87572d56f7e8
ndhaniff <ndhaniff@gmail.com>
2019-01-03 10:25:07 +0800

    update ipay88

--- resources/views/payment/paymentbackend.blade.php
+++ resources/views/payment/paymentbackend.blade.php
@@ -1,1 +1,44 @@
-<?php echo "RECEIVEOK"; ?>
+<?PHP
+$merchantcode = $_REQUEST["MerchantCode"];
+$paymentid  = $_REQUEST["PaymentId"];
+$refno  = $_REQUEST["RefNo"];
+$amount  = $_REQUEST["Amount"];
+$ecurrency  = $_REQUEST["Currency"];
+$remark  = $_REQUEST["Remark"];
+$transid  = $_REQUEST["TransId"];
+$authcode  = $_REQUEST["AuthCode"];
+$estatus  = $_REQUEST["Status"];
+$errdesc  = $_REQUEST["ErrDesc"];
+$signature  = $_REQUEST["Signature"];
+$ccname  = $_REQUEST["CCName"];
+$ccno   = $_REQUEST["CCNo"];
+$s_bankname  = $_REQUEST["S_bankname"];
+$s_country  = $_REQUEST["S_country"];
+?>
+
+//Add your programming code here
+<?php
+$expected_sign = $signature;
+$merchantcode = $this->merchantcode;
+$merchantkey = $this->merchantkey;
+
+$check_sign = '';
+$str = '';
+$HashAmount = '';
+$orderID = $refno;
+$paymentID = $paymentid;
+$paymentStatus = $estatus;
+$amountPaid = $amount;
+$transID = $transid;
+$remark = $remark;
+$errDesc = $errdesc;
+
+$HashAmount = str_replace(array(',','.'), "", $amountPaid);
+$str = $merchantkey . $merchantcode . $paymentID .trim(stripslashes($orderID)). $HashAmount . $ecurrency . $paymentStatus;
+$check_sign = hash('sha256', $str);
+
+if ($paymentStatus == "1" && $expected_sign == $check_sign) {
+  echo "RECEIVEOK";
+}
+
+?>
