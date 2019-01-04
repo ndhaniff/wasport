eeb0bd365ed74b6870c1de80d2eb25bb1421ff20
ndhaniff <ndhaniff@gmail.com>
2019-01-04 10:16:30 +0800

    testing backend payment

--- resources/views/payment/paymentbackend.blade.php
+++ resources/views/payment/paymentbackend.blade.php
@@ -1,5 +1,5 @@
 <?PHP
-/*$merchantcode = $_REQUEST["MerchantCode"];
+$merchantcode = $_REQUEST["MerchantCode"];
 $paymentid  = $_REQUEST["PaymentId"];
 $refno  = $_REQUEST["RefNo"];
 $amount  = $_REQUEST["Amount"];
@@ -13,23 +13,11 @@ $signature  = $_REQUEST["Signature"];
 $ccname  = $_REQUEST["CCName"];
 $ccno   = $_REQUEST["CCNo"];
 $s_bankname  = $_REQUEST["S_bankname"];
-$s_country  = $_REQUEST["S_country"];*/
-
-$merchantcode = 'M18793';
-$paymentid  = '31';
-$refno  = '10057';
-$amount  = '1.00';
-$ecurrency  = 'MYR';
-$remark  = 'Order for Very Miss Rabbit Virtual Run (Order ID: 10057)';
-$transid  = 'T201922481900';
-$estatus  = '1';
-$errdesc  = '';
-$signature  = 'd99d30691fa04a09528c1391d536e301e871fb66d601f61ab1914abba416ff4e';
+$s_country  = $_REQUEST["S_country"];
 ?>
 
 <?php
 $expected_sign = $signature;
-//$merchantcode = $merchantcode;
 $merchantkey = 'flICG0S4Ul';
 
 $check_sign = '';
@@ -50,5 +38,4 @@ $check_sign = hash('sha256', $str);
 if ($paymentStatus == "1" && $expected_sign == $check_sign) {
   echo "RECEIVEOK";
 }
-
 ?>
