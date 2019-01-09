f9b8edb1b9981ff44ec5f24a51de898a44dc12ad
ndhaniff <ndhaniff@gmail.com>
2019-01-09 08:39:02 +0800

    test route

--- routes/web.php
+++ routes/web.php
@@ -31,6 +31,7 @@ Route::group([
   Route::get('/contactus', 'Pages\ContactController@contactus')->name('contactus');
   Route::get('/registersuccess', 'Pages\HomeController@registersuccess')->name('registersuccess');
   //Route::get('/registerrace/{rid}', 'User\UserController@registerRace')->name('registerrace');
+  Route::get('/payment/ipay88/backendcallback', 'User\UserController@backendcallback')->name('backendcallback');
 
   //user
   Route::get('/dashboard', 'User\UserController@dashboard' )->name('user.dashboard');
@@ -166,7 +167,7 @@ Route::post('/ipay88/submitrace', 'API\IPay88Controller@submitRace')->name('ipay
 //iPay88
 Route::post('/payment/ipay88/callback', 'User\UserController@callback')->name('callback');
 //Route::post('/payment/ipay88/backendcallback', 'User\UserController@backendcallback')->name('backendcallback');
-Route::get('/payment/ipay88/backendcallback', 'User\UserController@backendcallback')->name('backendcallback');
+//Route::get('/payment/ipay88/backendcallback', 'User\UserController@backendcallback')->name('backendcallback');
 //Route::post('/ipay88/makepayment', 'API\IPay88Controller@makePayment')->name('ipay88.makepayment');
 
 //Contact Us
